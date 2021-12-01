5892dc5ac8c0741a871df01949294443c087ce2d
MatiasMarek94 <matias_marek@icloud.com>
2021-12-01 13:23:05 +0100

    turning a light on or off now saves the new state in the form of a powerunit

--- Services/LightService.cs
+++ Services/LightService.cs
@@ -20,26 +20,50 @@ namespace InnovativeSoftware.Services
             _powerUnitService = powerUnitService;
         }
 
-        public async Task<object> SwitchState(string userName, string token, Guid powerUnitID, bool turnOn)
+        public async Task<object> SwitchState(string userName, string token, Guid powerUnitId, bool turnOn)
         {
-            var powerUnit = await _powerUnitService.Get(new GetPowerUnitParameters { PowerUnitId = powerUnitID });
+            var powerUnit = await _powerUnitService.Get(new GetPowerUnitParameters { PowerUnitId = powerUnitId });
             var powerUnitManufacturer = powerUnit.Manufacturer;
 
             switch (powerUnitManufacturer)
             {
                 case PowerUnitManufacturer.PhilipsHue:
-                    return await _philipshueRepository.ChangeLightStatus(userName, token, powerUnit.DeviceId, turnOn);
+                    var result =
+                        await _philipshueRepository.ChangeLightStatus(userName, token, powerUnit.DeviceId, turnOn);
+                    if (result)
+                    {
+                        await updatePowerUnitOn(powerUnit, turnOn);
+                        Console.WriteLine("SwitchState update in switch if");
+                        return true;
+                    }
+
+                    break;
                 case PowerUnitManufacturer.Ikea:
                     return false;
                 default:
                     return false;
             }
+
+            return false;
         }
 
         public async Task<Dictionary<string, PhilipsHueLightDTO>> GetAllLights(string userName, string token)
         {
             var result = await _philipshueRepository.GetAllLights(userName, token);
             return result;
+        }
+
+        private async Task updatePowerUnitOn(PowerUnit powerUnit, bool isOn)
+        {
+            var updated = new UpdatePowerUnitParameters
+            {
+                Manufacturer = powerUnit.Manufacturer,
+                Name = powerUnit.Name,
+                On = isOn,
+                Tags = powerUnit.Tags,
+                DeviceId = powerUnit.DeviceId
+            };
+            await _powerUnitService.Update(powerUnit.PowerUnitId, updated);
         }
     }
 }
